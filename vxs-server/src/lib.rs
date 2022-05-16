c5fc7e97d50dc76c27a650ba2fe7d86f8536e32e
michaelgrigoryan25 <56165400+michaelgrigoryan25@users.noreply.github.com>
2022-05-16 19:27:18 +0000

    vxs-server: doc patch

--- vxs-server/src/lib.rs
+++ vxs-server/src/lib.rs
@@ -1,10 +1,13 @@
+pub mod rpc_queue;
+
 /// [VxServer] is a server implementation for vxs. It returns a struct,
 /// which provides different useful handlers, including for starting,
 /// stopping, etc.
 pub struct VxServer {
     /// vxs-server address must be a valid [ip address][ip-wiki], or [hostname][host-wiki].
     ///
-    /// [ip-wiki]: https://en.wikipedia.org/wiki/IP_ad    /// [host-wiki]: https://en.wikipedia.org/wiki/Hostname
+    /// [ip-wiki]: <https://en.wikipedia.org/wiki/IP_ad>    
+    /// [host-wiki]: <https://en.wikipedia.org/wiki/Hostname>
     pub(crate) address: String,
 
     /// vxs-server can function in a verbose mode, in case of which, everything, including
